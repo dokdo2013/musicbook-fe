62a4dae1469015b91085635a71ac939138cdb4b0
skymins04 <skymin0417@gmail.com>
2022-09-11 15:22:37 +0900

    edit next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
