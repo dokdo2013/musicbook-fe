70d13210d48d353668d5ccdc79a47b5324a9049e
skymins04 <skymin0417@gmail.com>
2022-11-10 16:58:02 +0900

    article.tsx의 styled-jsx를 chakra ui로 대체함

--- src/components/article/modules/article.tsx
+++ src/components/article/modules/article.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumb, BreadcrumbItem } from "@chakra-ui/react";
+import { Box, Breadcrumb, BreadcrumbItem } from "@chakra-ui/react";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { GLOBAL_PADDING_2, GLOBAL_PADDING_3, MUSICBOOK_URL_KEYS } from "@lib/constant";
 import Link from "next/link";
@@ -13,7 +13,15 @@ interface Props {
 export const Article: FC<Props> = ({ children, page, pageParam }) => {
   return (
     <>
-      <div className="article-wrap">
+      <Box
+        display="block"
+        overflowX="hidden"
+        overflowY="scroll"
+        w="full"
+        height="100%"
+        p={`${GLOBAL_PADDING_2}px`}
+        borderRight="1px solid #e2e8f0"
+      >
         <Breadcrumb
           spacing="8px"
           mb={`${GLOBAL_PADDING_3}px`}
@@ -46,19 +54,7 @@ export const Article: FC<Props> = ({ children, page, pageParam }) => {
           )}
         </Breadcrumb>
         {children}
-      </div>
-      <style jsx>{`
-        .article-wrap {
-          display: block;
-          overflow-x: hidden;
-          overflow-y: scroll;
-          width: 100%;
-          height: 100%;
-          padding: ${GLOBAL_PADDING_2}px;
-          background-color: #edf2f7;
-          border-right: 1px solid #e2e8f0;
-        }
-      `}</style>
+      </Box>
     </>
   );
 };
