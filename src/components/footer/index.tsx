ceb56df46d9fce4207c7594b4910c2459aefdd3d
skymins04 <skymin0417@gmail.com>
2022-11-12 16:16:08 +0900

    footer 다크모드 추가

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,50 +1,39 @@
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
 import { FOOTER_HEIGHT_PX, GLOBAL_PADDING_1, MAX_FRAME_WIDTH_PX } from "@lib/constant";
 import { useResponsive } from "@lib/hooks";
 import { FC } from "react";
 
 export const Footer: FC = () => {
   const { isMobile } = useResponsive();
+  const footerBgColor = useColorModeValue("gray.400", "gray.600");
 
   return (
     <>
-      <div className={`footer-wrap ${isMobile && "mobile"}`}>
-        <div className="content">
-          <div className="info">
+      <Flex
+        position="relative"
+        w="full"
+        height={`${FOOTER_HEIGHT_PX}px`}
+        p={0}
+        justifyContent="center"
+        alignItems="center"
+        bg={footerBgColor}
+      >
+        <Flex
+          position="relative"
+          w="full"
+          maxWidth={`${MAX_FRAME_WIDTH_PX}px`}
+          height="max-content"
+          p={`0 ${GLOBAL_PADDING_1}px`}
+          justifyContent="center"
+          alignItems="flex-start"
+          direction="column"
+        >
+          <Text fontSize="12px">
             © 2022 <span className="bold">https://musicbook.tv</span>. All rights reserved.
-          </div>
-          <div className="info">앨범아트의 저작권 일체는 음원 소유자에게 있습니다.</div>
-        </div>
-      </div>
-      <style jsx>{`
-        .footer-wrap {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          position: relative;
-          width: 100%;
-          height: ${FOOTER_HEIGHT_PX}px;
-          padding: 0;
-          background-color: #a0aec0;
-
-          .content {
-            --padding: ${GLOBAL_PADDING_1}px;
-            position: relative;
-            display: flex;
-            justify-content: center;
-            align-items: flex-start;
-            flex-direction: column;
-            width: 100%;
-            max-width: ${MAX_FRAME_WIDTH_PX}px;
-            height: max-content;
-            padding: 0 var(--padding);
-
-            .info {
-              font-size: 12px;
-              color: #e2e8f0;
-            }
-          }
-        }
-      `}</style>
+          </Text>
+          <Text fontSize="12px">앨범아트의 저작권 일체는 음원 소유자에게 있습니다.</Text>
+        </Flex>
+      </Flex>
     </>
   );
 };
